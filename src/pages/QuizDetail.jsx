92113910928cfd41f97493ae9e3f3c6b1422bccf
Pleng_Nuttida <66070059@kmitl.ac.th>
2026-03-15 19:45:27 +0700

    adjust pop up in QuizDetail.jsx

--- src/pages/QuizDetail.jsx
+++ src/pages/QuizDetail.jsx
@@ -111,6 +111,15 @@ export default function QuizDetail() {
                   >
                     <FaShareAlt />
                   </button>
+                  {copied && (
+                    <div className="fixed bottom-6 left-1/2 -translate-x-1/2
+                                    bg-black text-white
+                                    px-4 py-2 rounded-lg shadow-lg
+                                    text-sm z-50
+                                    animate-fade-in">
+                      Link copied to clipboard!
+                    </div>
+                  )}
                 </div>
 
                 {/* categories */}
